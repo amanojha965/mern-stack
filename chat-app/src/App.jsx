9bb4a56fa5efcb29a95ac920fa7eb6e3a1b9664c
amanojha965 <amanojha9650@gmail.com>
2026-03-28 03:45:01 +0530

    first commit

---
+++ chat-app/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    //" "<></>"for storing multiple values like multiple div's in one function 
+
+    <>
+      <div id="dada">
+        <h1>bda baap</h1>
+      </div>
+      <div id="dad">
+        <h1>App</h1>
+        <h3>hello world </h3>
+      </div>
+    </>
+  )
+}
+
+export default App
